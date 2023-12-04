be1721a8d3c767889cb533975fcd1484a01bb022
iamparth <parthgenius.gps@gmail.com>
2023-12-04 22:30:28 +0530

    initial commit

---
+++ snapgram/src/lib/validation/index.ts
@@ -0,0 +1,20 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+  name: z.string().min(2, { message: "Too short" }),
+  username: z.string().min(2, { message: "Too short" }),
+  email: z.string(),
+  password: z.string().min(8, { message: "Too short" }),
+});
+
+export const SigninValidation = z.object({
+  email: z.string(),
+  password: z.string().min(8, { message: "Too short" }),
+});
+
+export const PostValidation = z.object({
+  caption: z.string().min(5).max(2200),
+  file: z.custom<File[]>(),
+  location: z.string().min(2).max(100),
+  tags: z.string(),
+});
